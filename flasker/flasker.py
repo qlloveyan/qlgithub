868c2fa8d011324be7707224c8a8c23b059b97e5
quanli <13026148179@163.com>
2018-07-05 17:37:02 +0800

    flask主函数修改

--- flasker/flasker.py
+++ flasker/flasker.py
@@ -29,7 +29,7 @@ def not_found_page(error):
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
-def not_found_page(error):
+def server_error_page(error):
     return render_template('500.html'), 500
 
 @app.route('/index')
