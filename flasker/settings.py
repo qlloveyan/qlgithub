b79a622bef4d76e8cbcc648347b4ebdde6a61e3d
qlloveyan <13026148179@163.com>
2018-07-16 20:36:56 +0800

    配置信息注释添加

--- flasker/settings.py
+++ flasker/settings.py
@@ -1,8 +1,10 @@
 #-*- coding:utf-8 -*-
 class Config(object):
+    # 基本配置信息
     debug = False
     MYSQL_DATABASE_URI = 'mysql+mysqlconnector://root:123456@localhost:3306/flasker'
 
 class DevConfig(Config):
+    # 本地开发环境配置
     debug = True
     MYSQL_DATABASE_URI = 'mysql+pymysql://root:123456@localhost:3306/flasker'
